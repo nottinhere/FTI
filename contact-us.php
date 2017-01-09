99d4ac80cd74a3c6c70ae90a2cf9c491004da988
nottinhere <nottinhere@gmail.com>
2017-01-09 17:25:35 +0700

    Add contact.php

---
+++ contact-us.php
@@ -0,0 +1,90 @@
+<!doctype html>
+<html class="no-js" lang="th">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title>Contact Us | BRAIN การปฏิรูปธุรกิจและสร้างเครือข่ายนวัตกรรม</title>
+        <meta name="description" content="การปฏิรูปธุรกิจและสร้างเครือข่ายนวัตกรรมโดยสภาอุตสาหกรรมแห่งประเทศไทย">
+        <meta name="author" content="Find.File.Fine">
+
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" sizes="180x180">
+        <link rel="icon" type="image/png" href="/favicon-32.png" sizes="32x32">
+        <link rel="icon" type="image/png" href="/favicon-16.png" sizes="16x16">
+
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Prompt:400,600">
+        <link rel="stylesheet" href="css/main.css">
+        <script src="js/vendor/modernizr-2.8.3.min.js"></script>
+    </head>
+    <body class="page-contact-us">
+        <!--[if lt IE 9]>
+            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+        
+        <!-- Header -->
+        <header class="page-header__container">
+            <div class="page-header">
+                <a href="index.html" title="Back to BRAIN Home"><img class="logo" src="images/logo-brain.svg" alt="Brain logo" width="110"></a>
+                <nav class="page-nav">
+                    <ul>
+                        <li><a href="index.html">HOME</a></li>
+                        <li><a href="whats-brain.html">What's Brain</a></li>
+                        <li><a href="brain-speakers-schedule.html">BRAIN Speakers &amp; Schedule</a></li>
+                        <li><a href="brain-list.html">BRAIN List</a></li>
+                        <li><a href="brain-director.html">BRAIN Director</a></li>
+                        <li><a href="brain-gallery.html">BRAIN Gallery</a></li>
+                        <li><a class="page-nav--active" href="contact-us.html">Contact Us</a></li>
+                    </ul>
+                </nav>
+            </div><!-- end page-header -->
+        </header>
+
+        <!-- Contact -->
+        <div class="contact__container">
+            <section class="contact">
+                <h2>Contact</h2>
+                <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15503.653859274404!2d100.5590833!3d13.7236887!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x230edac05ae63ab7!2sQueen+Sirikit+National+Convention+Center.!5e0!3m2!1sen!2sth!4v1479961990634" width="500" height="350" frameborder="0" style="border:0" allowfullscreen></iframe>
+                <div class="contact-info">
+                    <h3>สภาอุตสาหกรรมแห่งประเทศไทย</h3>
+                    <p>
+                        โซน ซี ชั้น 4 ศูนย์การประชุมแห่งชาติสิริกิติ์ 60 ถนนรัชดาภิเษกตัดใหม่ แขวงคลองเตย<br>
+                        เขตคลองเตย กรุงเทพฯ 10110
+                    </p>
+                    <h3>ติดต่อสอบถามเพิ่มเติม</h3>
+                    <p>
+                        <strong>คุณศิรินารถ เคลือบกำเหนิด</strong><br>
+                        <strong>โทร:</strong> <a href="tel:+6623451067">02-345-1067</a><br>
+                        <strong>E-mail:</strong> <a href="mailto:sirinartk@off.fti.or.th">sirinartk@off.fti.or.th</a>
+                    </p>
+                    <p>
+                        <strong>คุณธิดารัตน์ ตระกูลไชยพฤกษ์</strong><br>
+                        <strong>โทร:</strong> <a href="tel:+6623451067">02-345-1067</a><br>
+                        <strong>E-mail:</strong> <a href="mailto:t.thidaratt@gmail.com">t.thidaratt@gmail.com</a></p>
+                </div>
+            </section>
+        </div>
+
+        <!-- Footer -->
+        <footer class="page-footer__container">
+            <div class="page-footer">
+                <p>
+                    Contact Us | Tel: <a href="tel:+6623451067">02 345 1067</a><br>
+                    E-mail: <a href="mailto:thidarattt@off.fti.or.th">thidarattt@off.fti.or.th</a>, <a href="mailto:sirinartk@off.fti.or.th">sirinartk@off.fti.or.th</a><br>
+                    <small>Copyright by Business Revolution And Innovation Network</small>
+                </p>
+            </div>
+        </footer>
+
+        <script src="https://code.jquery.com/jquery-1.12.0.min.js"></script>
+        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.12.0.min.js"><\/script>')</script>
+        <script src="js/plugins.js"></script>
+
+        <script>
+            (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+            function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+            e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+            e.src='https://www.google-analytics.com/analytics.js';
+            r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+            ga('create','UA-88003638-1','auto');ga('send','pageview');
+        </script>
+    </body>
+</html>
